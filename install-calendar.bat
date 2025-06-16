052559501e56e09edbbb9e9d333032fb8908f02a
youcisla <chehbouby@yahoo.fr>
2025-06-16 12:50:23 +0200

    #7

--- install-calendar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Installing React Native Calendars...
-cd /d "%~dp0"
-npm install react-native-calendars
-echo Installation complete!
-pause
--- start-dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting SkillSwap Development Server...
-cd /d "%~dp0"
-npm install
-npm start
-pause
